0984a29b44304c8cde3100cd429f9c2f84884784
syarwanarief <ariefsyarwan@gmail.com>
2020-07-25 14:51:17 +0700

    Absensi Siswa Revisi 4

--- app/src/main/java/mobile/project/absensisiswa/MenuGuru/AbsensiAction.java
+++ app/src/main/java/mobile/project/absensisiswa/MenuGuru/AbsensiAction.java
@@ -1,35 +1,44 @@
 package mobile.project.absensisiswa.MenuGuru;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.telephony.SmsManager;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 import java.util.Locale;
 
+import mobile.project.absensisiswa.MenuAdmin.InputSiswa;
 import mobile.project.absensisiswa.R;
 import mobile.project.absensisiswa.SmsGateway;
 
 public class AbsensiAction extends AppCompatActivity {
-    EditText nama, mataPelajaran, NoHP;
-    Spinner kelas, bidangStudi;
+    EditText nama, NoHP;
+    Spinner kelas, bidangStudi, mataPelajaran;
 
     private DatePickerDialog datePickerDialog;
     private SimpleDateFormat dateFormatter;
@@ -45,13 +54,38 @@ public class AbsensiAction extends AppCompatActivity {
 
         nama = (EditText) findViewById(R.id.Abnama);
         NoHP = (EditText) findViewById(R.id.AbNoHP);
-        mataPelajaran = (EditText) findViewById(R.id.AbmataPelajaran);
+        mataPelajaran = (Spinner) findViewById(R.id.AbmataPelajaran);
         kelas = (Spinner) findViewById(R.id.Abkelas);
         bidangStudi = (Spinner) findViewById(R.id.AbbidangStudi);
 
         viewTgl = (EditText) findViewById(R.id.tglAbsen);
         dateFormatter = new SimpleDateFormat("dd-MM-yyyy", Locale.US);
         btnTgl = (Button) findViewById(R.id.btnTgl);
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Daftar_Pelajaran").child("All");
+        reference.keepSynced(true);
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                final List<String> pelajaran = new ArrayList<String>();
+
+                for (DataSnapshot areaSnapshot : snapshot.getChildren()) {
+                    String StringPel = areaSnapshot.child("Mata_Pelajaran").getValue(String.class);
+                    pelajaran.add(StringPel);
+                }
+
+                ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(AbsensiAction.this, android.R.layout.simple_spinner_item, pelajaran);
+                arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                mataPelajaran.setAdapter(arrayAdapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
         btnTgl.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -106,7 +140,7 @@ public class AbsensiAction extends AppCompatActivity {
 
         String stringNama = nama.getText().toString();
         String stringKelas = kelas.getSelectedItem().toString();
-        String stringPelajaran = mataPelajaran.getText().toString();
+        String stringPelajaran = mataPelajaran.getSelectedItem().toString();
         String stringStudi = bidangStudi.getSelectedItem().toString();
         String stringTgl = viewTgl.getText().toString();
         String stringNoHP = NoHP.getText().toString();
@@ -114,7 +148,7 @@ public class AbsensiAction extends AppCompatActivity {
         try{
             SmsManager smgr = SmsManager.getDefault();
             smgr.sendTextMessage(stringNoHP,null,"Informasi Absensi anak anda, yang bernama "+stringNama+
-                    "di Mata Pelajaran "+stringPelajaran+" pada tgl "+stringTgl+" adalah 'Alpha' ",null,null);
+                    " di Mata Pelajaran "+stringPelajaran+" pada tgl "+stringTgl+" adalah 'Alpha' ",null,null);
             Toast.makeText(AbsensiAction.this, "Informasi Absensi Berhasil Dikirim Kepada Orangtua Wali", Toast.LENGTH_SHORT).show();
         }
         catch (Exception e){
@@ -136,6 +170,7 @@ public class AbsensiAction extends AppCompatActivity {
         keyKet.setValue("Alpha");
 
         nama.setText("");
+        NoHP.setText("");
 
     }
 
@@ -143,7 +178,7 @@ public class AbsensiAction extends AppCompatActivity {
 
         String stringNama = nama.getText().toString();
         String stringKelas = kelas.getSelectedItem().toString();
-        String stringPelajaran = mataPelajaran.getText().toString();
+        String stringPelajaran = mataPelajaran.getSelectedItem().toString();
         String stringStudi = bidangStudi.getSelectedItem().toString();
         String stringTgl = viewTgl.getText().toString();
         String stringNoHP = NoHP.getText().toString();
@@ -151,7 +186,7 @@ public class AbsensiAction extends AppCompatActivity {
         try{
             SmsManager smgr = SmsManager.getDefault();
             smgr.sendTextMessage(stringNoHP,null,"Informasi Absensi anak anda, yang bernama "+stringNama+
-                    "di Mata Pelajaran "+stringPelajaran+" pada tgl "+stringTgl+" adalah 'Izin' ",null,null);
+                    " di Mata Pelajaran "+stringPelajaran+" pada tgl "+stringTgl+" adalah 'Izin' ",null,null);
             Toast.makeText(AbsensiAction.this, "Informasi Absensi Berhasil Dikirim Kepada Orangtua Wali", Toast.LENGTH_SHORT).show();
         }
         catch (Exception e){
@@ -173,13 +208,14 @@ public class AbsensiAction extends AppCompatActivity {
         keyKet.setValue("Izin");
 
         nama.setText("");
+        NoHP.setText("");
     }
 
     public void hadir(View view) {
 
         String stringNama = nama.getText().toString();
         String stringKelas = kelas.getSelectedItem().toString();
-        String stringPelajaran = mataPelajaran.getText().toString();
+        String stringPelajaran = mataPelajaran.getSelectedItem().toString();
         String stringStudi = bidangStudi.getSelectedItem().toString();
         String stringTgl = viewTgl.getText().toString();
         String stringNoHP = NoHP.getText().toString();
@@ -187,7 +223,7 @@ public class AbsensiAction extends AppCompatActivity {
         try{
             SmsManager smgr = SmsManager.getDefault();
             smgr.sendTextMessage(stringNoHP,null,"Informasi Absensi anak anda, yang bernama "+stringNama+
-                    "di Mata Pelajaran "+stringPelajaran+" pada tgl "+stringTgl+" adalah 'Hadir' ",null,null);
+                    " di Mata Pelajaran "+stringPelajaran+" pada tgl "+stringTgl+" adalah 'Hadir' ",null,null);
             Toast.makeText(AbsensiAction.this, "Informasi Absensi Berhasil Dikirim Kepada Orangtua Wali", Toast.LENGTH_SHORT).show();
         }
         catch (Exception e){
@@ -209,13 +245,14 @@ public class AbsensiAction extends AppCompatActivity {
         keyKet.setValue("Hadir");
 
         nama.setText("");
+        NoHP.setText("");
     }
 
     public void sakit(View view) {
 
         String stringNama = nama.getText().toString();
         String stringKelas = kelas.getSelectedItem().toString();
-        String stringPelajaran = mataPelajaran.getText().toString();
+        String stringPelajaran = mataPelajaran.getSelectedItem().toString();
         String stringStudi = bidangStudi.getSelectedItem().toString();
         String stringTgl = viewTgl.getText().toString();
         String stringNoHP = NoHP.getText().toString();
@@ -223,7 +260,7 @@ public class AbsensiAction extends AppCompatActivity {
         try{
             SmsManager smgr = SmsManager.getDefault();
             smgr.sendTextMessage(stringNoHP,null,"Informasi Absensi anak anda, yang bernama "+stringNama+
-                    "di Mata Pelajaran "+stringPelajaran+" pada tgl "+stringTgl+" adalah 'Sakit' ",null,null);
+                    " di Mata Pelajaran "+stringPelajaran+" pada tgl "+stringTgl+" adalah 'Sakit' ",null,null);
             Toast.makeText(AbsensiAction.this, "Informasi Absensi Berhasil Dikirim Kepada Orangtua Wali", Toast.LENGTH_SHORT).show();
         }
         catch (Exception e){
@@ -245,13 +282,7 @@ public class AbsensiAction extends AppCompatActivity {
         keyKet.setValue("Sakit");
 
         nama.setText("");
-
-        try {
-            // hentikan sms server
-            smsGateway.stop();
-        } catch (IOException e) {
-            Log.e(getClass().getName(), e.getMessage(), e);
-        }
+        NoHP.setText("");
 
     }
 
